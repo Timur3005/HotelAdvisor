8f4bbff8bece4bc6736541eccb601dc532befe0e
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2023-11-19 11:51:44 +0300

    add domain's second model and edit first

--- app/src/main/java/com/example/domain/HotelEntity.kt
+++ app/src/main/java/com/example/domain/HotelEntity.kt
@@ -5,6 +5,9 @@ data class HotelEntity(
     val name: String,
     val address: String,
     val minPrice: Int,
+    val priceFor: String,
+    val rating: Double,
+    val ratingName: String,
     val hotelDescription: String,
     val peculiarities: List<String>,
     val images: List<String>
