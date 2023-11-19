3f6980c70bde54f654bd4077c1501f25b1f71900
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2023-11-19 13:05:58 +0300

    add binding

--- app/src/main/java/com/example/hoteladvisor/presentation/MainActivity.kt
+++ app/src/main/java/com/example/hoteladvisor/presentation/MainActivity.kt
@@ -3,10 +3,25 @@ package com.example.hoteladvisor.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hoteladvisor.R
+import com.example.hoteladvisor.databinding.ActivityMainBinding
+import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding ?: throw RuntimeException("binding isn't exist")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
 }
